710e4c167f587f081f97490c2cfc58a182f0b16a
UsaidPro <usaidpro@gmail.com>
2017-03-04 19:27:12 -0600

    Encoders on DriveBase

--- src/org/usfirst/frc/team5431/robot/DriveBase.java
+++ src/org/usfirst/frc/team5431/robot/DriveBase.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team5431.robot;
 
 import org.usfirst.frc.team5431.robot.constants;
+
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.Encoder;
 import com.kauailabs.navx.frc.AHRS;
 import com.ctre.CANTalon;
 
@@ -9,19 +12,25 @@ public class DriveBase{
     CANTalon bLeft;
     CANTalon masterRight;
     CANTalon masterLeft;
+    Encoder rightEncoder;
+    Encoder leftEncoder;
     AHRS ahrs;
     
 	public DriveBase(){
 		masterRight = new CANTalon(constants.masterRightId);
-		masterRight.setFeedbackDevice(CANTalon.FeedbackDevice.EncRising);
+//		masterRight.setFeedbackDevice(CANTalon.FeedbackDevice.EncRising);
     	masterLeft = new CANTalon(constants.masterLeftId);
-    	masterLeft.setFeedbackDevice(CANTalon.FeedbackDevice.EncRising);
+//    	masterLeft.setFeedbackDevice(CANTalon.FeedbackDevice.EncRising);
     	bRight = new CANTalon(constants.bRightId);
     	bRight.changeControlMode(CANTalon.TalonControlMode.Follower);
     	bRight.set(constants.masterRightId);
     	bLeft = new CANTalon(constants.bLeftId);
     	bLeft.changeControlMode(CANTalon.TalonControlMode.Follower);
     	bLeft.set(constants.masterLeftId);
+    	rightEncoder = new Encoder(0, 1, false, EncodingType.k4X);
+    	rightEncoder.setDistancePerPulse(0.00589294059473);
+    	leftEncoder = new Encoder(2, 3, false, EncodingType.k4X);
+    	leftEncoder.setDistancePerPulse(0.00589294059473);
 	}
 	
 	public void drive(double left, double right){
@@ -41,12 +50,12 @@ public class DriveBase{
 	
 	public double leftEncoder()
 	{
-		return masterLeft.getEncPosition() * 0.00589294059473;
+		return masterLeft.get();
 	}
 	
 	public double rightEncoder()
 	{
-		return masterRight.getEncPosition() * 0.00589294059473;
+		return masterRight.get();
 	}
 	
 	public double getYaw()
